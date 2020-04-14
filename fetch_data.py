0e7012dffac2e8fd197ffa2de310026aecfaee41
Caleb Schmidt <schmical@oregonstate.edu>
2020-04-13 20:46:04 -0400

    Added initial by country data fetch stub

--- fetch_data.py
+++ fetch_data.py
@@ -2,9 +2,26 @@ import requests
 
 
 def fetch_by_state_data():
-    resp = requests.get('https://www.cdc.gov/coronavirus/2019-ncov/map-cases-us.json')
+    url = 'https://www.cdc.gov/coronavirus/2019-ncov/map-cases-us.json'
+    resp = requests.get(url)
 
     if not resp.ok:
         resp.raise_for_status()
 
+    return resp.json()
+
+
+def fetch_by_country_data():
+    url = 'https://services.arcgis.com/5T5nSi527N4F7luB/arcgis/rest/services/Cases_by_country_Plg_V3/FeatureServer/0/query'
+    params = {
+        'f': 'json',
+        'where': '1=1',
+        'returnGeometry': False,
+        'spatialRel': 'esriSpatialRelIntersects',
+        'outFields': '*',
+        'orderByFields': 'cum_conf desc',
+        'cacheHint': 'true',
+    }
+
+    resp = requests.get(url, params=params)
     return resp.json()
