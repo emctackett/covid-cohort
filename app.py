4f99641f21d7a4caa95abbdcbe0ffd02ce24efb0
Caleb Schmidt <schmical@oregonstate.edu>
2020-04-14 21:12:18 -0400

    added route stubs for pages

--- app.py
+++ app.py
@@ -10,6 +10,30 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/info')
+def info():
+    # Not passing any variables to the template yet
+    return render_template('info.html')
+
+
+@app.route('/reminders')
+def reminders():
+    # Not passing any variables to the template yet
+    return render_template('reminders.html')
+
+
+@app.route('/social-distancing')
+def social_distancing():
+    # Not passing any variables to the template yet
+    return render_template('social_distancing.html')
+
+
+@app.route('/should-i-go-out')
+def should_i_go_out():
+    # Not passing any variables to the template yet
+    return render_template('should_i_go_out.html')
+
+
 if __name__ == '__main__':
     # 0.0.0.0 makes it accessible over the network
     # If running locally, navigate to 'localhost:<port #>' in browser
