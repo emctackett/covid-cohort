6aa26610eb206e6a7473f5a31c64883ddc01f4c6
Elizabeth Tackett <emctackett@gmail.com>
2020-04-18 14:41:17 -0500

    add /learn route and js file

---
+++ front-end/src/Learn.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "./Navbar";
+import SimpleFooter from "./Footer";
+
+export default function Learn() {
+  return (
+    <div>
+      <Navigation />
+      <SimpleFooter />
+    </div>
+  );
+}
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import Learn from "./Learn";
 import * as serviceWorker from "./serviceWorker";
 
 const routing = (
   <Router>
     <div>
       <Route exact path="/" component={App} />
+      <Route exact path="/learn" component={Learn} />
       {/* <Route path="/about" component={} />
       <Route path="/learn" component={} />
       <Route path="/safety" component={} />
