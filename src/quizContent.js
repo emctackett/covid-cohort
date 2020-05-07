14dd580e6d03457593bee930a2899f3df46da4af
Elizabeth Tackett <emctackett@gmail.com>
2020-05-07 15:39:33 -0500

    populate quizContent data with drafted questions and answers

--- src/quizContent.js
+++ src/quizContent.js
@@ -1,4 +1,87 @@
-const quiz = {};
+const quiz = {
+  "quizTitle": "Should I go Out?",
+  "quizSynopsis": "If you are feeling unsure whether it's safe to leave home, take this quick quiz that is based off of the COVID-19 guidelines.",
+  "questions": [
+    {
+      "question": "In the past 48 hours, have you had a fever of 100.4° or above?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Yes",
+        "No"
+      ],
+      "correctAnswer": "2",
+      "messageForIncorrectAnswer": "Stay home. We recommend you call your physician.",
+    },
+    {
+      "question": "Are you short of breath?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Yes",
+        "No"
+      ],
+      "correctAnswer": "2",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Stay home.",
+    },
+    {
+      "question": "Do you have a cough?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Yes",
+        "No"
+      ],
+      "correctAnswer": "2",
+      "messageForIncorrectAnswer": "Stay home.",
+    },
+    {
+      "question": "Are you experiencing chills or shaking?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Yes",
+        "No"
+      ],
+      "correctAnswer": "2",
+      "messageForIncorrectAnswer": "Stay home.",
+    },
+    {
+      "question": "Are you experiencing any of the following: muscle pain, headache, sore throat, loss of taste or smell?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Yes",
+        "No"
+      ],
+      "correctAnswer": "2",
+      "messageForIncorrectAnswer": "Stay home.",
+    },
+    {
+      "question": "In the past two weeks, have you had contact with someone you know has been diagnosed with COVID-19?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Yes",
+        "No"
+      ],
+      "correctAnswer": "2",
+      "messageForIncorrectAnswer": "Stay home",
+    },
+    {
+      "question": "Will you wear a cloth face covering while out?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Yes",
+        "No"
+      ],
+      "correctAnswer": "1",
+      "messageForIncorrectAnswer": "Stay home"
+    }
+  ]
+};
 
 
 export default quiz;
