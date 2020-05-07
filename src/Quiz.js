4d152d6500b95ff274839aac469262ca29ebcdce
Elizabeth Tackett <emctackett@gmail.com>
2020-05-07 10:59:29 -0500

    Quiz component initialized

---
+++ src/Quiz.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import {
+  Anchor,
+  Heading,
+  Grommet,
+  Main,
+  Box,
+  Paragraph,
+} from "grommet";
+import { grommet } from "grommet/themes";
+
+
+export default function Quiz() {
+  return (
+    
+  );
+}
