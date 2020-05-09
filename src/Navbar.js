9ad7398263aeb377569784c8060b39665834538d
Laura Jiang <jianglau@oregonstate.edu>
2020-05-09 13:26:10 -0700

    fix responsive navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -42,12 +42,42 @@ const CollapsableNav = () => (
             <Menu
               label="Navigate"
               items={[
-                { label: "data", onClick: () => {} },
-                { label: "learn", onClick: () => {} },
-                { label: "avoid", onClick: () => {} },
-                { label: "safety", onClick: () => {} },
-                { label: "remind", onClick: () => {} },
-                { label: "help", onClick: () => {} },
+                {
+                  label: "data",
+                  onClick: (event) => {
+                    window.location.href = "/data";
+                  },
+                },
+                {
+                  label: "learn",
+                  onClick: (event) => {
+                    window.location.href = "/learn";
+                  },
+                },
+                {
+                  label: "avoid",
+                  onClick: (event) => {
+                    window.location.href = "/avoid";
+                  },
+                },
+                {
+                  label: "safety",
+                  onClick: (event) => {
+                    window.location.href = "/safety";
+                  },
+                },
+                {
+                  label: "remind",
+                  onClick: (event) => {
+                    window.location.href = "/remind";
+                  },
+                },
+                {
+                  label: "help",
+                  onClick: (event) => {
+                    window.location.href = "/help";
+                  },
+                },
               ]}
             />
           ) : (
