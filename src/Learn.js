cc94bd8c078c83013b920f0a1c122346b6520d37
MikaylaFr <45703548+MikaylaFr@users.noreply.github.com>
2020-05-17 14:40:09 -0700

    Refactor - reduce lines

--- src/Learn.js
+++ src/Learn.js
@@ -3,14 +3,7 @@ import Navigation from "./Navbar";
 import SimpleFooter from "./Footer";
 import ResizedIframe from "./EmbedPlayer.js";
 
-import {
-  Anchor,
-  Heading,
-  Grommet,
-  Main,
-  Box,
-  Paragraph,
-} from "grommet";
+import {Anchor, Heading, Grommet, Main, Box, Paragraph} from "grommet";
 import { grommet } from "grommet/themes";
 
 const GridLayout = () => {
@@ -23,33 +16,18 @@ const GridLayout = () => {
         </Heading>
         <br />
         <Box alignContent="start" width="xlarge">
-        <Paragraph
-          fill={true}
-          margin={{ left: "xlarge", right: "xlarge" }}
-          size="large"
-        >
-          <Anchor
-            href="https://www.cdc.gov/coronavirus"
-            color="#000000"
-          >
-            According to the Centers for Disease Control and Prevention,{" "}
+        <Paragraph fill={true} margin={{ left: "xlarge", right: "xlarge" }} size="large">
+          According to the{" "}
+          <Anchor href="https://www.cdc.gov/coronavirus" color="#000000">
+            Centers for Disease Control and Prevention
           </Anchor>
-          the following are known symptoms of COVID-19. Symptoms can range from mild (or no symptoms) to severe ilness.
+          , the following are known symptoms of COVID-19. Symptoms can range from mild (or no symptoms) to severe ilness.
           <br /><br />
           Note that symptoms can appear up to two weeks after exposure to the virus.
         </Paragraph>
         </Box>
-        <Box
-          alignContent="start"
-          width="xlarge"
-          pad={{ left: "large", right: "large" }}
-          alignSelf="center"
-        >
-          <Paragraph
-            fill={true}
-            margin={{ left: "xlarge", right: "xlarge" }}
-            size="large"
-          >
+        <Box alignContent="start" width="xlarge" pad={{ left: "large", right: "large" }} alignSelf="center">
+          <Paragraph fill={true} margin={{ left: "xlarge", right: "xlarge" }} size="large">
           ‣	Fever
           <br />
           <br />
@@ -81,31 +59,16 @@ const GridLayout = () => {
         </Heading>
         <br />
         <Box alignContent="start" width="xlarge">
-          <Paragraph
-            fill={true}
-            margin={{ left: "xlarge", right: "xlarge" }}
-            size="large"
-          >
-            <Anchor
-              href="https://www.cdc.gov/coronavirus"
-              color="#000000"
-            >
-              According to the CDC
+          <Paragraph fill={true} margin={{ left: "xlarge", right: "xlarge" }} size="large">
+            According to the{" "}
+            <Anchor href="https://www.cdc.gov/coronavirus" color="#000000">
+              CDC
             </Anchor>
             {" "}, most people will be able to recover from COVID-19 at home.  Follow the below guidelines if you experience symptoms.
           </Paragraph>
         </Box>
-        <Box
-          alignContent="start"
-          width="xlarge"
-          pad={{ left: "large", right: "large" }}
-          alignSelf="center"
-        >
-        <Paragraph
-          fill={true}
-          margin={{ left: "xlarge", right: "xlarge" }}
-          size="large"
-        >
+        <Box alignContent="start" width="xlarge" pad={{ left: "large", right: "large" }} alignSelf="center">
+        <Paragraph fill={true} margin={{ left: "xlarge", right: "xlarge" }} size="large">
               ‣	Stay at home if you are sick. Leave your home only to seek medical attention, if necessary.
               <br />
               <br />
@@ -146,31 +109,15 @@ const GridLayout = () => {
         </Heading>
         <br />
         <Box alignContent="start" width="xlarge">
-          <Paragraph
-            fill={true}
-            margin={{ left: "xlarge", right: "xlarge" }}
-            size="large"
-          >
+          <Paragraph fill={true} margin={{ left: "xlarge", right: "xlarge" }} size="large">
             If you are unsure whether or not you need medical attention, call your primary care provider or local health department.{" "}
-            <Anchor
-              href="https://www.cdc.gov/coronavirus/2019-ncov/symptoms-testing/symptoms.html"
-              color="#000000"
-            >
+            <Anchor href="https://www.cdc.gov/coronavirus/2019-ncov/symptoms-testing/symptoms.html" color="#000000">
               Per the CDC,
             </Anchor>{" "}seek medical attention immediately if you experience any of the below symptoms.
           </Paragraph>
         </Box>
-        <Box
-          alignContent="start"
-          width="xlarge"
-          pad={{ left: "large", right: "large" }}
-          alignSelf="center"
-        >
-        <Paragraph
-          fill={true}
-          margin={{ left: "xlarge", right: "xlarge" }}
-          size="large"
-        >
+        <Box alignContent="start" width="xlarge" pad={{ left: "large", right: "large" }} alignSelf="center">
+        <Paragraph fill={true} margin={{ left: "xlarge", right: "xlarge" }} size="large">
             ‣	Trouble breathing
             <br />
             <br />
@@ -193,19 +140,10 @@ const GridLayout = () => {
             Learn More About COVID-19
           </Heading>
           <Box pad="large" align="center">
-            <ResizedIframe
-              src="https://www.youtube.com/embed/BtN-goy9VOY"
-            />
-            <Paragraph
-              fill={true}
-              margin={{ left: "xlarge", right: "xlarge" }}
-              size="large"
-            >
+            <ResizedIframe src="https://www.youtube.com/embed/BtN-goy9VOY"/>
+            <Paragraph fill={true} margin={{ left: "xlarge", right: "xlarge" }} size="large">
               Citations for the information presented in this video are presented{" "}
-              <Anchor
-                href="https://sites.google.com/view/sourcescorona"
-                color="#000000"
-              >here.</Anchor>{" "}
+              <Anchor href="https://sites.google.com/view/sourcescorona" color="#000000">here.</Anchor>{" "}
             </Paragraph>
           </Box>
         </Box>
