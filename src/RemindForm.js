f18dcbcd40b072a04be989282f1d8f78a7071bac
MikaylaFr <45703548+MikaylaFr@users.noreply.github.com>
2020-05-17 14:11:17 -0700

    Mikayla Refactor HW7 - reduce lines

--- src/RemindForm.js
+++ src/RemindForm.js
@@ -2,36 +2,30 @@
   import ApiCalendar from 'react-google-calendar-api';
   
   export default class DoubleButton extends React.Component {
-      constructor(props) {
-        super(props);
+    constructor(props) {
+    	super(props);
         this.handleItemClick = this.handleItemClick.bind(this);
-      };
+    };
       
-      handleItemClick(event: SyntheticEvent<any>, name: string): void {
+    handleItemClick(event: SyntheticEvent<any>, name: string): void {
         if (name === 'sign-in') {
-          ApiCalendar.handleAuthClick();
+        	ApiCalendar.handleAuthClick();
         } else if (name === 'sign-out') {
-          ApiCalendar.handleSignoutClick();
+          	ApiCalendar.handleSignoutClick();
         }
       }
 
-      render(): ReactNode {
+    render(): ReactNode {
         return (
-	<div>
-              <button
-                  onClick={(e) => this.handleItemClick(e, 'sign-in')}
-              >
-                sign-in
-              </button>
-              <button
-                  onClick={(e) => this.handleItemClick(e, 'sign-out')}
-              >
-                sign-out
-              </button>
-	</div>
-          );
-      }
-  }
+		<div>
+            <button onClick={(e) => this.handleItemClick(e, 'sign-in')}>
+                sign-in</button>
+            <button onClick={(e) => this.handleItemClick(e, 'sign-out')}>
+            	sign-out</button>
+		</div>
+        );
+    }
+}
 
 {/*
 import React from "react";
